000d667afd13ccf5e7ff09fb43667eed0299b455
Denzel Sapatin <dknsapatin@gmail.com>
2022-05-01 01:18:29 -0400

    Adding auth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.get("/api/test", () => {
 app.use(express.json());
 // User route
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
